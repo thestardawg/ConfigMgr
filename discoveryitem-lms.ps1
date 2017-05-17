3638c913bfe616a531faeb3d132d523a93fabf5a
thestardawg <thestardawg@gmail.com>
2017-05-16 18:56:04 -0700

    Add files via upload

---
+++ discoveryitem-lms.ps1
@@ -0,0 +1,12 @@
+﻿#SCCM CB ConfigItem / Remediation to turn LMS off and disable from auto startup.
+
+#Discovery - Script - String.
+(get-wmiobject -namespace root\CIMv2 -class Win32_Service | where-object -filterscript {$_.Name -eq “LMS”}).started
+
+#Remediation
+$service = get-wmiobject -namespace root\cimv2 -class Win32_Service | where-object -filterscript {$_.Name -eq “lms”}
+$service.changestartmode(“disabled”)
+$service.stopservice()
+
+#Compliance Rule
+#Condition = False.  Remediation = Yes.
